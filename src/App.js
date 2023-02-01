0ba2758d19900ab1e23378734214ec12872236a5
ronak-d <ronakdodeja73@gamil.com>
2023-02-01 21:50:11 +0530

    newheader

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import NewHeader from "./Components/NewHeader/NewHeader";
 import AllRoutes from "./Components/Routes/AllRoutes";
-
+import "./Components/CSS/AppCSS.css";
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="AppCSS">
         <NewHeader />
         <AllRoutes />
       </div>
--- src/Components/NewHeader/NewHeader.jsx
+++ src/Components/NewHeader/NewHeader.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { AppBar, Toolbar, Typography } from "@mui/material";
+import { Container } from "@mui/system";
 
 const NewHeader = () => {
-  return <div>NewHeader</div>;
+  return (
+    <AppBar color="transparent" position="static">
+      <Container>
+        <Toolbar>
+          <Typography>Crypto Tracker</Typography>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 };
 
 export default NewHeader;
