0f76f4b97a6875b489244c66d3700690138ea8d1
ronak-d <ronakdodeja73@gamil.com>
2023-02-12 18:04:18 +0530

    god saved my code

--- src/Components/Pages/CoinsPage.jsx
+++ src/Components/Pages/CoinsPage.jsx
@@ -8,6 +8,7 @@ import CryptoCharts from "../ChartsComponent/CryptoCharts";
 // css
 import "../CSS/CoinsPage.css";
 import { Box, Container } from "@mui/material";
+import Typography from "@mui/material/Typography";
 
 const CoinsPage = () => {
   const { id } = useParams();
@@ -29,7 +30,18 @@ const CoinsPage = () => {
   return (
     <>
       <Box className="coinContainer">
-        <Box className="coinInfo">coin</Box>
+        <Box className="coinInfo">
+          {
+            <Container>
+              <img src={coin?.image.large} alt="" />
+              <p className="heading">{coin?.id}</p>
+              <p variant="h6" component="h6">
+                {/* {coin?.description.en.split(". ")[0]} */}
+              </p>
+            </Container>
+          }
+        </Box>
+
         <Box className="coinCharts">
           <CryptoCharts />
         </Box>
