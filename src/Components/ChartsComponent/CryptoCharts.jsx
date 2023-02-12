fc193b6f3117db9775bf6a9533f15aa8686282a5
ronak-d <ronakdodeja73@gamil.com>
2023-02-13 04:44:56 +0530

    Crypro page Intiated with tools

--- src/Components/ChartsComponent/CryptoCharts.jsx
+++ src/Components/ChartsComponent/CryptoCharts.jsx
@@ -1,11 +1,42 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { HistoricalChart } from "../Config/api";
+import { CryptoState } from "../Context/CryptoContext";
+import CircularProgress from "@mui/material/CircularProgress";
 
-const CryptoCharts = () => {
-  return (
+// chart.js
+import { Line } from "react-chartjs-2";
+
+const CryptoCharts = ({ coin }) => {
+  const [historicaldata, setHistoricalData] = useState();
+  const [days, setDays] = useState(1);
+  const { currency, symbol } = CryptoState();
+
+  const fetchCharts = async () => {
+    const { data } = await axios.get(HistoricalChart(coin.id, days, currency));
+    // console.log("data", data);
+    setHistoricalData(data.prices);
+    console.log(historicaldata);
+  };
+
+  useEffect(() => {
+    fetchCharts();
+  }, [currency, days]);
+
+  return !historicaldata ? (
+    <CircularProgress color="success" />
+  ) : (
     <>
-      <p>heelo</p>
+      <Line data={data} />
     </>
   );
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+export const data = {
+  labels,
+  datasets: [],
 };
 
 export default CryptoCharts;
