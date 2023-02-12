f39eeee783a05fbe35b2757470d765b5560eefab
ronak-d <ronakdodeja73@gamil.com>
2023-02-13 05:29:39 +0530

    charts done

--- src/Components/ChartsComponent/CryptoCharts.jsx
+++ src/Components/ChartsComponent/CryptoCharts.jsx
@@ -6,6 +6,17 @@ import CircularProgress from "@mui/material/CircularProgress";
 
 // chart.js
 import { Line } from "react-chartjs-2";
+import { yellow } from "@mui/material/colors";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  Legend,
+  LinearScale,
+  LineElement,
+  PointElement,
+  Title,
+  Tooltip,
+} from "chart.js";
 
 const CryptoCharts = ({ coin }) => {
   const [historicaldata, setHistoricalData] = useState();
@@ -16,27 +27,50 @@ const CryptoCharts = ({ coin }) => {
     const { data } = await axios.get(HistoricalChart(coin.id, days, currency));
     // console.log("data", data);
     setHistoricalData(data.prices);
-    console.log(historicaldata);
+    console.log("historicaldata", historicaldata);
   };
 
   useEffect(() => {
     fetchCharts();
   }, [currency, days]);
 
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
   return !historicaldata ? (
     <CircularProgress color="success" />
   ) : (
     <>
-      <Line data={data} />
+      <Line
+        data={{
+          // x- axis
+          labels: historicaldata.map((coin) => {
+            let date = new Date(coin[0]);
+            let time =
+              date.getHours() > 12
+                ? `${date.getHours() - 12}:${date.getMinutes()} PM`
+                : `${date.getHours()}:${date.getMinutes()} AM`;
+            return days === 1 ? time : date.toLocaleDateString();
+          }),
+          // y-axis
+          datasets: [
+            {
+              data: historicaldata.map((coin) => coin[1]),
+              label: `Price ( Past ${days} Days ) in ${currency}`,
+              borderColor: "#EEBC1D",
+            },
+          ],
+        }}
+      />
     </>
   );
-};
-
-const labels = ["January", "February", "March", "April", "May", "June", "July"];
-
-export const data = {
-  labels,
-  datasets: [],
 };
 
 export default CryptoCharts;
